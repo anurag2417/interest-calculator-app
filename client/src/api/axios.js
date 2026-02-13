3382d861ba46cdbed0730f6de83bd1ad493884f7
Anurag Kumar <anuragakn18@gmail.com>
2026-02-14 04:23:17 +0530

    Refactor Axios configuration to dynamically set base URL for development and production environments

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
-// CHANGE THIS: Use the Render URL you just copied
+// Automatically choose the right URL
+// If running 'npm run dev' -> Use Localhost
+// If running on Vercel -> Use Render URL
+const BASE_URL = import.meta.env.MODE === 'development' 
+    ? 'http://localhost:5000/api' 
+    : 'https://interest-calculator-app-cx9o.onrender.com';
+
 const instance = axios.create({
-    baseURL: 'https://interest-calculator-app-cx9o.onrender.com' 
+    baseURL: BASE_URL
 });
 
 export default instance;
