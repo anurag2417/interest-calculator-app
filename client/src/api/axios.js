95ad1824c7d7f73539cadc68bb9d903442fde5bd
Anurag Kumar <anuragakn18@gmail.com>
2026-02-14 04:16:27 +0530

    Update Axios base URL to point to the correct Render deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // CHANGE THIS: Use the Render URL you just copied
 const instance = axios.create({
-    baseURL: 'https://interest-calculator-api.onrender.com/api' 
+    baseURL: 'https://interest-calculator-app-cx9o.onrender.com' 
 });
 
 export default instance;
